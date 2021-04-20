7164327c92cef77aabe53016a2a5964c0c71475d
debirizk <debirizky45@gmail.com>
2021-04-20 19:12:30 +0800

    save react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { Cards, CardsGlobal,  CountryPicker } from "./Components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import {fetchDailyData} from "./api";
+import coronaImage from "./images/image.png";
+import Home from "./Components/Home/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <br />
+        <br />
+        <CardsGlobal data={data} country={"Global"}/>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Cards data={data} country={country} />
+        <Home />
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
